e29879684d417696a4ed04125e935242d1cdca51
トモユキ 苗村 <t.namura017140@gmail.com>
2020-03-11 00:28:33 +0900

    hoge

--- test/python_test.py
+++ test/python_test.py
@@ -1,3 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.plot(np.array([1, 3, 2]))
+plt.show()
+
 print("hello world")
 
 a = 1
--- update.sh
+++ update.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 # copy .vimrc to ~
-echo "Info: copy .vimrc."
+echo "Info: copy .vimrc to HOME."
 cp ./.vimrc ~/
 wait
 
 # install plugin
 echo "Info: install new plugins."
 vim +PluginInstall +qall
+wait
+
+# copy .vimrc to ~
+echo "Info: copy .tmux.conf to HOME."
+cp ./.tmux.conf ~/
 wait
 
 echo "Info: done."
