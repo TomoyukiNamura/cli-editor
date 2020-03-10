b1882950aca1d2b12cc71876ec5bd276d22c5c6d
トモユキ 苗村 <t.namura017140@gmail.com>
2020-03-10 23:52:45 +0900

    first commit 2

--- install.sh
+++ install.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-function command_exists {
-    command -v "$1" > /dev/null;
-}
+#function command_exists {
+#    command -v "$1" > /dev/null;
+#}
+#
+## install git vim 3
+#if ! command_exists git ; then
+#    apt update
+#    wait
+#    apt install -y git vim
+#    wait
+#else
+#    echo "Info: command git already exists."
+#fi
 
-# install git vim 3
-if ! command_exists git ; then
-    apt update
-    wait
-    apt install -y git vim
-    wait
-else
-    echo "Info: command git already exists."
-fi
+
+# install use commands
+apt update
+wait
+apt install -y git vim tmux
+wait
 
 # make dir ~/. vim/bundle
 if [ ! -d ~/.vim/bundle ];then
